74b34936f8623daa82072eed794bae25d2a65735
Shiz <hi@shiz.me>
2022-08-22 21:14:37 +0200

    scripts: add {get-boot2,set-boot2}

---
+++ scripts/get-boot2.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+usage() {
+	printf "usage: %s [-h] [-p PROFILE] <nandfile> <outdir>\n" "$0" >&2
+}
+
+profile=retail
+
+while getopts hp: opt; do
+	case "$opt" in
+	h) usage; exit 0;;
+	p) profile="$OPTARG";;
+	?) usage; exit 255;;
+	esac
+done
+shift $(($OPTIND - 1))
+[ $# -eq 2 ] || { usage; exit 255; }
+nandfile="$1"
+outdir="$2"
+
+case "$profile" in
+retail|*-retail)
+	tmd_chain=Root-CA00000001-CP00000004
+	tik_chain=Root-CA00000001-XS00000003
+	;;
+dev|*-dev)
+	tmd_chain=Root-CA00000002-CP00000007
+	tik_chain=Root-CA00000002-XS00000006
+	;;
+*)	printf "%s: unknown profile %s\n" "$0" "$profile" >&2; exit 255;;
+esac
+
+runtool() {
+	tool="$1"; shift
+	python3 "$tool".py "$@"
+}
+
+nandfile="$1"
+outdir="$2"
+
+echo ">> extract"
+runtool tsoprocky -p "$profile" extract-boot2 "$nandfile" "$outdir"/boot2.crt "$outdir"/boot2.stmd "$outdir"/boot2.stik "$outdir"/boot2.ebin
+runtool tweezer -p "$profile" import "$outdir"/boot2.crt
+echo ">> verify"
+runtool tweezer -p "$profile" verify -k $tmd_chain "$outdir"/boot2.stmd "$outdir"/boot2.tmd
+runtool tweezer -p "$profile" verify -k $tik_chain "$outdir"/boot2.stik "$outdir"/boot2.tik
+echo ">> decrypt"
+runtool tweezer -p "$profile" decrypt -i 0 "$outdir"/boot2.tmd "$outdir"/boot2.tik "$outdir"/boot2.ebin "$outdir"/boot2.bin
---
+++ scripts/set-boot2.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+set -e
+
+usage() {
+	printf "usage: %s [-h] [-p PROFILE] <nandfile> <indir>\n" "$0" >&2
+}
+
+profile=retail
+
+while getopts hp: opt; do
+	case "$opt" in
+	h) usage; exit 0;;
+	p) profile="$OPTARG";;
+	?) usage; exit 255;;
+	esac
+done
+shift $(($OPTIND - 1))
+[ $# -eq 2 ] || { usage; exit 255; }
+nandfile="$1"
+outdir="$2"
+
+case "$profile" in
+retail|*-retail)
+	tmd_chain=Root-CA00000001-CP00000004
+	sign_args=
+	;;
+dev|*-dev)
+	tmd_chain=Root-CA00000002-CP00000007
+	sign_args=-P
+	;;
+*)	printf "%s: unknown profile %s\n" "$0" "$profile" >&2; exit 255;;
+esac
+
+runtool() {
+	tool="$1"; shift
+	python3 "$tool".py "$@"
+}
+
+nandfile="$1"
+indir="$2"
+
+echo ">> update"
+runtool tweezer -p "$profile" update -i 0 "$indir"/boot2.tmd "$indir"/boot2.bin "$indir"/boot2.new.tmd
+echo ">> encrypt"
+runtool tweezer -p "$profile" encrypt -i 0 "$indir"/boot2.new.tmd "$indir"/boot2.tik "$indir"/boot2.bin "$indir"/boot2.new.ebin
+echo ">> sign"
+runtool tweezer -p "$profile" sign -k $tmd_chain -f $sign_args "$indir"/boot2.new.tmd "$indir"/boot2.new.stmd
+echo ">> insert"
+runtool tsoprocky -p "$profile" insert-boot2 "$nandfile" "$indir"/boot2.crt "$indir"/boot2.new.stmd "$indir"/boot2.stik "$indir"/boot2.new.ebin
